a96910086c85308719d96b50bc23b33f23ae2938
afspicciati <afspicciati@gmail.com>
2026-03-18 13:07:26 -0700

    test

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from shiny import ui, render, App, Inputs, reactive
 import pandas as pd
 import seaborn as sns
 
+#test test test
 # constants
 basic_lands = ['plains', 'island', 'swamp', 'mountain', 'forest']
 
